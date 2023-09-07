84118d13ba3dcf1a4a7e8730f3bf6dd9d852107d
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-07 11:50:53 -0300

    Update bubble_sort.py

---
+++ bubble_sort/bubble_sort.py
@@ -0,0 +1,16 @@
+def sort(array):
+    for final in range(len(array), 0, -1):
+        exchanging = False
+
+        for current in range(0, final -1):
+            if array[current] > array[current +1]:
+                array[current], array[current +1] = array[current +1], array[current]
+                exchanging = True
+
+        if not exchanging:
+            break
+
+
+array = sorted([8, 7, 5, 3, 1, 2, 9, 6, 4])
+sort(array)
+print(array)
