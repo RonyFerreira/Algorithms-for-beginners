585fb5d2af2d256dddcb82cdb7200f91e8dc9515
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-07 11:23:08 -0300

    selection_sort.py

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,18 +1,8 @@
 
-def selection_sort(lista):
-    n= len(lista)
-    for j in range(n-1):
-        min_index = j
-        for i in range(j, n):
-            if lista[i] < lista[min_index]:
-                min_index = i
-
-
-        if lista[j] > lista[min_index]:
-            aux = lista[j]
-            lista[j] = lista[min_index]
-            lista[min_index] = aux
-
+def sort(array):
+    for index in range(0, len(array)):
+        min_index = index
+        
 
            
 
