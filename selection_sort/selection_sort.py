8f54e2bde1517f42fc701a506614ddfaf46aa3e0
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-07 10:35:45 -0300

    numbers_list.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,12 +1,19 @@
 
+def selection_sort(lista):
+    n= len(lista)
+    for j in range(n-1):
+        min_index = j
+        for i in range(j, n):
+            if lista[i] < lista[min_index]:
+                min_index = i
 
 
-lista = [7, 5, 1, 8, 3]
+        if lista[j] > lista[min_index]:
+            aux = lista[j]
+            lista[j] = lista[min_index]
+            lista[min_index] = aux
 
-n= len(lista)
-minimo = lista[0]
-for i in range(n):
-    if lista[i] < minimo:
-        minimo = lista[i]
 
-print(minimo)
+     
+
+
